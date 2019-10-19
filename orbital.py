6388e41a5fc1bf101869ed8da482b4a92fba3e67
Justin Thomas <thomascjustin@gmail.com>
2019-10-19 18:05:58 -0400

    Add error handling to general news pages

--- orbital.py
+++ orbital.py
@@ -22,23 +22,23 @@ def open_general(driver):
 
     move_to_new_tab("tab2", "https://www.cnn.com/")
     try:
-        mainSection = driver.find_elements_by_class_name("zn-banner")[0]
-        mainSection.click()
+        instructions = 'driver.find_elements_by_class_name("zn-banner")[0].click()'
+        handle_error(instructions)
     except Exception as e:
-        mainSection = driver.find_elements_by_class_name("cn-list-hierarchical-xs")[0].find_elements_by_tag_name('li')[0]
-        mainSection.click()
+        instructions = 'driver.find_elements_by_class_name("cn-list-hierarchical-xs")[0].find_elements_by_tag_name("li")[0].click()'
+        handle_error(instructions)
 
     move_to_new_tab("tab3", "https://www.foxnews.com/")
-    mainSection = driver.find_elements_by_class_name("main-content")[0].find_elements_by_class_name("story-1")[0]
-    mainSection.click()
+    instructions = 'driver.find_elements_by_class_name("main-content")[0].find_elements_by_class_name("story-1")[0].click()'
+    handle_error(instructions)
 
     move_to_new_tab("tab4", "https://www.bbc.com/")
-    mainSection = driver.find_elements_by_class_name("module--promo")[0].find_elements_by_class_name("media-list__item--1")[0]
-    mainSection.click()
+    instructions = 'driver.find_elements_by_class_name("module--promo")[0].find_elements_by_class_name("media-list__item--1")[0].click()'
+    handle_error(instructions)
 
     move_to_new_tab("tab5", "https://www.huffpost.com/")
-    mainSection = driver.find_element_by_id("zone-main").find_elements_by_class_name("card__headline__text")[0]
-    mainSection.click()
+    instructions = 'driver.find_element_by_id("zone-main").find_elements_by_class_name("card__headline__text")[0].click()'
+    handle_error(instructions)
 
 def open_tech(driver):
     driver.get("https://news.ycombinator.com/")
